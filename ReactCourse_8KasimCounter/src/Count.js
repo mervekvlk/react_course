3db556fc389b62776937450d8568bedb0b42185d
mervekvlk <94161605+mervekvlk@users.noreply.github.com>
2023-11-08 10:29:24 +0300

    React Counter

---
+++ ReactCourse_8KasimCounter/src/Count.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+function Count(){ 
+    const [sayac, setSayac]=useState(0);
+
+    const artir= () => {
+        if (sayac!==10){
+            setSayac(sayac+1);
+            }
+    }
+
+    const azalt=() => {
+        if(sayac!==0){
+        setSayac(sayac-1);
+        }
+    }
+
+
+    return(
+        <div>
+            <h2> Count: {sayac}</h2>
+            <button className="btn btn-info " onClick={artir}>Artır</button>
+            <button className="btn btn-danger" onClick={azalt}>Azalt</button>
+        </div>
+    );
+
+}
+
+export default Count;
